18a30eb52c69231eb04747f57c3c408de8a2f0fd
anhvanmaster <anhvanmaster@gmail.com>
2019-03-25 16:34:52 +0700

    view home html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in!232
                 </div>
             </div>
         </div>
